d6493454e8c15e7d40d8ffc85bf426e4b09b6935
AverkinSergei <averkin22@gmail.com>
2022-07-24 12:28:23 +0500

    refactor ml models

--- ml/models.py
+++ ml/models.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
@@ -7,10 +8,51 @@ from surprise import Dataset
 from surprise import accuracy
 from surprise import Reader
 from surprise.model_selection import train_test_split
+from sqlalchemy import create_engine
 
 
 def change_string(s):
     return ' '.join(s.replace(' ', '').replace('-', '').split('|'))
+
+
+class AbstractLoader(ABC):
+    """
+    Abstract class for ml data loading.
+    """
+    @abstractmethod
+    def load(self, *args, **kwargs) -> pd.DataFrame:
+        pass
+
+
+class CsvLoader(AbstractLoader):
+    """
+    Csv data loader implementation.
+    """
+    def load(self, path: str) -> pd.DataFrame:
+        return pd.read_csv(path)
+
+
+class PgLoader(AbstractLoader):
+    """
+    Postgres data loader implementation.
+    """
+    def __init__(self, dsn: str):
+        self.connection = create_engine(dsn)
+
+    def load(self) -> pd.DataFrame:
+        return pd.read_sql_query('select * from "film_work"', con=self.connection)
+
+
+class AbstractModel(ABC):
+    """
+    Base class for ML model.
+    """
+    def __init__(self, loader: AbstractLoader):
+        self.loader = loader
+
+    @abstractmethod
+    def load_data(self):
+        pass
 
 
 class ContentBasedModel(object):
@@ -19,7 +61,7 @@ class ContentBasedModel(object):
     """
 
     def __init__(self,
-                 neighbors_count: int = 7,
+                 neighbors_count: int = 10,
                  jobs_count: int = -1,
                  metric: str = 'euclidean'):
         self.movies = pd.read_csv('data/movies.csv')
